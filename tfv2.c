d54a3e0473f341d503460355a163cd635096144e
motoshira <51407064+motoshira@users.noreply.github.com>
2026-01-16 20:15:22 +0900

    Add tfv2

---
+++ tfv2.c
@@ -0,0 +1,49 @@
+#include <gtk/gtk.h>
+
+static void app_activate(GApplication *app) {
+  GtkWidget *win;
+  GtkWidget *scrolled_window;
+  GtkWidget *text_view;
+  GtkTextBuffer *text_buffer;
+  gchar *text;
+
+  text =
+    "Once upon a time, there was an old man who was called Taketori-no-Okina. "
+    "It is a japanese word that means a man whose work is making bamboo baskets.\n"
+    "One day, he went into a hill and found a shining bamboo. "
+    "\"What a mysterious bamboo it is!,\" he said. "
+    "He cut it, then there was a small cute baby girl in it. "
+    "The girl was shining faintly. "
+    "He thought this baby girl is a gift from Heaven and took her home.\n"
+    "His wife was surprized at his story. "
+    "They were very happy because they had no children. "
+    ;
+
+  win = gtk_application_window_new(GTK_APPLICATION(app));
+  gtk_window_set_title(GTK_WINDOW(win), "Taketori");
+  gtk_window_set_default_size(GTK_WINDOW(win), 400, 300);
+
+  text_view = gtk_text_view_new();
+  text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+  gtk_text_buffer_set_text(text_buffer, text, -1);
+  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(text_view), GTK_WRAP_WORD_CHAR);
+
+  scrolled_window = gtk_scrolled_window_new();
+
+  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scrolled_window), text_view);
+
+  gtk_window_set_child(GTK_WINDOW(win), scrolled_window);
+
+  gtk_window_present(GTK_WINDOW(win));
+}
+
+int main (int argc, char *argv[]) {
+  GtkApplication *app;
+  int stat;
+
+  app = gtk_application_new("com.github.motoshira.tfv2", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK (app_activate), NULL);
+  stat = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+  return stat;
+}
